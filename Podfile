4da38ba38764add36979f755d3a1fec0f631a40f
Sangdon Lee <sangdon.lee@pixo.co>
2020-07-09 13:37:13 +0900

    Setup Texture

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+use_frameworks!
+
+pod "Texture"
+
+target 'TextureExamples' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+end
+
+target 'TextureStudy' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+
+end
---
+++ TextureExamples/TextureExamples.h
@@ -0,0 +1,19 @@
+//
+//  TextureExamples.h
+//  TextureExamples
+//
+//  Created by WonderPlug on 2020/07/09.
+//  Copyright © 2020 PIXO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TextureExamples.
+FOUNDATION_EXPORT double TextureExamplesVersionNumber;
+
+//! Project version string for TextureExamples.
+FOUNDATION_EXPORT const unsigned char TextureExamplesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextureExamples/PublicHeader.h>
+
+
